7282b59713d1c2f99ae4a7249885cb59755007c1
Valerii Petryniak <valerii15298@gmail.com>
2026-02-15 02:09:05 +0200

    setup dependencies update on sync

--- sync.bash
+++ sync.bash
@@ -1,16 +1,17 @@
 set -e
 
-rm -rf src/components/ui
+rm -rf src/components/ui pnpm-lock.yaml node_modules
+jq 'del(.peerDependencies)' package.json > tmp.json && mv tmp.json package.json
+
+pnpm install
+pnpm add @tailwindcss/typography tailwindcss
 
 # https://ui.shadcn.com/docs/installation/manual
-
-mv package.json package.json.bak
-cat package.json.bak | jq '.dependencies = {}' > package.json
-rm package.json.bak
-pnpm install
-
-pnpm add class-variance-authority clsx tailwind-merge lucide-react tw-animate-css react-resizable-panels @base-ui/react @tailwindcss/typography
+pnpm add class-variance-authority clsx tailwind-merge lucide-react tw-animate-css
 pnpx shadcn@latest add --all
 
 node fix-imports.js
+
+jq '.peerDependencies = .dependencies | del(.dependencies)' package.json > tmp.json && mv tmp.json package.json
+
 pnpm tsc --noEmit
